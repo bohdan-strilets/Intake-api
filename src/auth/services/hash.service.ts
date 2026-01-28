8b88146e69b4f0acecbbd3f473d7bb473ba0150c
BS <bohdan.strilets@gmail.com>
2026-01-28 16:53:24 +0100

    refactor(hash): renamed PasswordService to HashService

---
+++ src/auth/services/hash.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class HashService {
+  private readonly saltRounds: number;
+
+  constructor(config: ConfigService) {
+    this.saltRounds = Number(config.get<number>('BCRYPT_SALT_ROUNDS'));
+  }
+
+  async hash(plain: string): Promise<string> {
+    return bcrypt.hash(plain, this.saltRounds);
+  }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(plain, hash);
+  }
+}
