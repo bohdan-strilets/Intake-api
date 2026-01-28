5a40f3b2f8b98d065752ff6017e365b7fcd797e9
BS <bohdan.strilets@gmail.com>
2026-01-28 14:37:02 +0100

    feat(auth): connected Auth and Users modules to App

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth';
 import { EnvModule } from './common/config';
 import { DatabaseModule } from './common/database';
+import { UsersModule } from './users';
 
 @Module({
-  imports: [EnvModule, DatabaseModule],
+  imports: [EnvModule, DatabaseModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
