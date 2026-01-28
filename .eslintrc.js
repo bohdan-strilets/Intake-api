b23ca8285dbe12a9247e422298283d01c04ce64a
BS <bohdan.strilets@gmail.com>
2026-01-28 02:16:53 +0100

    chore: setup eslint and prettier and remove tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,35 @@
 module.exports = {
+  root: true,
   parser: '@typescript-eslint/parser',
+
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
   ],
-  root: true,
+
   env: {
     node: true,
     jest: true,
   },
+
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+
+    'prettier/prettier': 'error',
   },
 };
