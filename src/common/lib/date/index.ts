c4c409d90fca9fd70c7aaa809381291b4d37ad25
BS <bohdan.strilets@gmail.com>
2026-02-01 20:17:34 +0100

    feat(date): add new function for transform ms to days

--- src/common/lib/date/index.ts
+++ src/common/lib/date/index.ts
@@ -1,2 +1,3 @@
 export { daysToMs } from './days-to-ms';
+export { msToDays } from './ms-to-days';
 export { normalizeDate } from './normalize-date';
---
+++ src/common/lib/date/ms-to-days.ts
@@ -0,0 +1,3 @@
+export const msToDays = (ms: number): number => {
+  return ms / (1000 * 60 * 60 * 24);
+};
