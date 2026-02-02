2d2fff293aeab53a9d3d3dfd5cd33645caa89c68
BS <bohdan.strilets@gmail.com>
2026-02-02 17:57:48 +0100

    chore(api): freeze public day details API contracts

--- src/day-details/day-details.controller.ts
+++ src/day-details/day-details.controller.ts
@@ -1,17 +1,32 @@
 import { Auth } from '@app/auth/decorators';
 import { CurrentUserId } from '@app/common/decorators';
+import { ErrorResponseDto } from '@app/common/errors/dto';
 import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 import { DayDetailsService } from './day-details.service';
 import { DayDetailsResponseDto } from './dto';
 
 @Auth()
+@ApiTags('Day Details')
+@ApiBearerAuth('access-token')
 @Controller('day-details')
 export class DayDetailsController {
   constructor(private readonly dayDetailsService: DayDetailsService) {}
 
   @Get(':date')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get day details by date' })
+  @ApiOkResponse({ type: DayDetailsResponseDto })
+  @ApiBadRequestResponse({ type: ErrorResponseDto })
+  @ApiUnauthorizedResponse({ type: ErrorResponseDto })
   getDayDetails(
     @CurrentUserId() userId: string,
     @Param('date') date: string,
--- src/day-details/dto/day-details-response.dto.ts
+++ src/day-details/dto/day-details-response.dto.ts
@@ -1,7 +1,11 @@
 import { DayResponseDto } from '@app/days/dto';
 import { FoodResponseDto } from '@app/food/dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class DayDetailsResponseDto {
+  @ApiProperty({ type: DayResponseDto })
   day: DayResponseDto;
+
+  @ApiProperty({ type: [FoodResponseDto] })
   food: FoodResponseDto[];
 }
